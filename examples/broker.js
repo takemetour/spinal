cad4fa8a85dc43ced8bf6082aeb2423144a2f15c
Sira Sujjinanont <hunt@jitta.com>
2015-06-12 03:36:20 +0700

    Broker.stop()

--- examples/broker.js
+++ examples/broker.js
@@ -2,3 +2,7 @@ var broker = require('../').Broker();
 broker.start(function(socket){
   console.log('Spinal:Broker listening...'+socket.port)
 });
+
+// setTimeout(function(){
+//   broker.stop()
+// }, 7000)
