7a0586d22337c2eb371e82354b8f502fa46a5903
Sira Sujjinanont <hunt@jitta.com>
2015-07-08 05:38:37 +0700

    Update broker.js example

--- examples/broker.js
+++ examples/broker.js
@@ -2,8 +2,8 @@ var Broker = require('../').Broker
 var broker = new Broker({
   redis: 6379
 })
-broker.start(process.env.PORT, function(){
-  console.log('Spinal:Broker listening...'+this.port)
+broker.start(process.env.PORT || 7557, function(){
+  console.log('Spinal:Broker listening...' + this.port)
 })
 
 // setTimeout(function(){
