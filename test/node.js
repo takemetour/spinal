90b46d0ef3eb63fc0619e634b57028c9962ffc5e
Chonlatee Jumratsee <neng@jitta.com>
2015-10-21 17:34:49 -0700

    should not error when res empty or undefined

--- test/node.js
+++ test/node.js
@@ -301,6 +301,33 @@ describe('Node', function() {
       })
     })
 
+    it('Should not error when response empty', function(done) {
+      spinal.provide('jump', function(arg, res){
+        res()
+      })
+      spinal.start(function(){
+        spinal.call('jump', 'ok', function(err, data) {
+          assert.isNull(err)
+          assert.isNull(data);
+          done()
+        })
+      })
+    })
+
+    it('Should not error when response undefined first parameter', 
+    function(done) {
+      spinal.provide('jump', function(arg, res){
+        res(undefined)
+      })
+      spinal.start(function(){
+        spinal.call('jump', 'ok', function(err, data) {
+          assert.isNull(err)
+          assert.isNull(data);
+          done()
+        })
+      })
+    })
+
   })
 
   describe('Call', function() {
